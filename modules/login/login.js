17ad78fd569060dec79788a4c81910e0792e5fcf
Evan Almloff <evanalmloff@gmail.com>
2025-03-11 14:52:14 -0500

    try again

--- modules/login/login.js
+++ modules/login/login.js
@@ -18,7 +18,7 @@ Module.register("login", {
 
 	generateQrCode() {
 		const password = uuidv4();
-		this.ids = { id: "1337", password };
+		this.ids = { id: "02DF056B-09A4-4E1E-B8D0-05E216CFA6A9", password };
 
 		this.sendSocketNotification("START_BLE", this.ids)
 		this.updateDom();
